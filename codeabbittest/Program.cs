ca851041f986c01dfc437ede7f89394e5bc69546
Per Dickner <per.dickner@visma.com>
2024-09-17 21:20:53 +0200

    test again

--- codeabbittest/Program.cs
+++ codeabbittest/Program.cs
@@ -1,3 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Review this!");
+
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+
+
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
+Console.WriteLine("Review this!");
