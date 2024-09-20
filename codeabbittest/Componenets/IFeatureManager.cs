5fdd8f640d89bd6692d520230a5d94a36051985a
Per Dickner <per.dickner@visma.com>
2024-09-20 14:55:06 +0200

    test

---
+++ codeabbittest/Componenets/IFeatureManager.cs
@@ -0,0 +1,11 @@
+﻿namespace codeabbittest.Componenets
+{
+    using codeabbittest.Enums;
+    using System.Collections.Generic;
+
+    public interface IFeatureManager
+    {
+        bool IsFeatureEnabled(List<string> ids, FeatureFlag flag);
+        bool IsFeatureEnabledForAll(FeatureFlag flag);
+    }
+}
---
+++ codeabbittest/Enums/FeatureFlag.cs
@@ -0,0 +1,7 @@
+namespace codeabbittest.Enums
+{
+    public enum FeatureFlag
+    {
+        TestFeature = 0,
+    }
+}
