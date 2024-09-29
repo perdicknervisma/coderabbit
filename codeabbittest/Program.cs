673639a0b04a611b1296e20536a6727d290abba9
Per Dickner <per.dickner@visma.com>
2024-09-29 10:15:06 +0200

    test FeatureFlagcoverage

--- codeabbittest/Program.cs
+++ codeabbittest/Program.cs
@@ -1,8 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+bool featureflag = true;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Review this!");
 Console.WriteLine("Review this!");
 Console.WriteLine("Review this!");
+
+if (featureflag)
+{
+    Console.WriteLine("This code seems to be hidden behind a faked featureflag");
+}
+else
+{
+    Console.WriteLine("This code seems to be hidden behind a faked featureflag");
+}
+
+Console.WriteLine("HOW ABOUNT THIS CODE");
